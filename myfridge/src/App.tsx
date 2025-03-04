6c0c5127b38d91d574add77a337024694d0cd08a
kayyalexandre <143910758+kayyalexandre@users.noreply.github.com>
2025-03-04 16:48:39 -0500

    final coomit

--- myfridge/src/App.tsx
+++ myfridge/src/App.tsx
@@ -1,11 +1,22 @@
+import {Routes, Route } from 'react-router-dom';
+
 import './globals.css';
+import SignupForm from "./_auth/forms/SignupForm.tsx";
+import SigninForm from "./_auth/forms/SigninForm.tsx";
+import {Home} from "./_root/pages";
 
 const App = () => {
-    return(
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
+        return(
+            <main className="flex h-screen">
+                <Routes>
+                    {/* public routes */}
+                    <Route path="/sign-in" element={SigninForm/>} />
+                    <Route path="/sign-up" element={SignupForm/>} />
+                    {/* private routes */}
+                    <Route index element={<Home/>}/>
+                </Routes>
+            </main>
+        )
 }
 
 export default App
---
+++ myfridge/src/_auth/forms/AuthLayout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthoLayout = ()=> {
+    return (
+        <div>AuthLayout</div>
+    )
+}
+
+export default AuthoLayout
