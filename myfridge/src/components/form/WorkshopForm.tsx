235d9d1b16e893a2ea2b0c66b4420ea78fcdb78a
kayyalexandre <143910758+kayyalexandre@users.noreply.github.com>
2025-03-25 12:09:38 -0400

    final coomit

--- myfridge/src/components/form/WorkshopForm.tsx
+++ myfridge/src/components/form/WorkshopForm.tsx
@@ -74,7 +74,7 @@ const WorkshopForm = ({ workshop }: WorkshopFormProps) => {
 
     return (
         <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
+            <Form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-9 w-full max-w-5xl">
                 <FormField
                     control={form.control}
                     name="title"
@@ -159,4 +159,4 @@ const WorkshopForm = ({ workshop }: WorkshopFormProps) => {
                     )}
                 />
 
-                <Fo
+
